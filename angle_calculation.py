5872caf09a8c83c39559000077bb2f74a6dedea2
CharlieMonk <charlie@charliemonk.net>
2017-01-22 16:42:11 -0500

    Improving documentation

--- angle_calculation.py
+++ angle_calculation.py
@@ -1,12 +1,14 @@
 import math
 
 distance = float(input("How far? (use meters) "))
-#print(distance)
-#Height in meters
+# print(distance)
+# Height in meters
 height = 7.0
-#Initial velocity in meters/sec
+# Initial velocity in meters/sec
 initial_velocity = 600
 angle = None
+# Using variables to find different parts of the final equation to minimize possibility that I'll make a
+# mistake translating a huge mathematical equation to Python
 g = -9.8
 p0 = initial_velocity**2
 p1 = 2*(p0-g*height)
@@ -17,15 +19,16 @@ p3 = g*distance
 print p3
 
 if(p1>p2):
-	p4_1 = (p0 + math.sqrt(p1-p2))/p3
-	p4_2 = (p0 - math.sqrt(p1-p2))/p3
-	
-	theta_1 = math.atan(p4_1)
-	theta_2 = math.atan(p4_2)
-
-	print("Theta 1 is: " + str(theta_1))
-	print("Theta 2 is: " + str(theta_2))
+    p4_1 = (p0 + math.sqrt(p1-p2))/p3
+    p4_2 = (p0 - math.sqrt(p1-p2))/p3
+    # Finding angles
+    theta_1 = math.atan(p4_1)
+    theta_2 = math.atan(p4_2)
+    # Outputting angles
+    print("Theta 1 is: " + str(theta_1))
+    print("Theta 2 is: " + str(theta_2))
 else:
-	print("Error")
-	
+    # This occurs if the solution is an imaginary number, meaning that there's no angle that can put the ball in the
+    # hole from the given distance
+    print("Error")
 
